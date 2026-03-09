349e58bb91690bc35317601d3aed863444e2cdb8
ronis <ronaldo.luizcp@gmail.com>
2026-03-09 16:37:35 -0300

    feat: Docker env

--- src/main/java/com/example/tasksapi/TasksApiApplication.java
+++ src/main/java/com/example/tasksapi/TasksApiApplication.java
@@ -1,13 +1,12 @@
 package com.example.tasksapi;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:environment.properties")
-@PropertySource("classpath:sensitive.conf")
+@PropertySource(value = "classpath:sensitive.conf", ignoreResourceNotFound = true)
 public class TasksApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(TasksApiApplication.class, args);
